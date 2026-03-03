d86b99d27a59c27caf360a01cd70943fd99c1788
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-03-03 21:10:29 +0530

    Basic Setup Done

--- simple-qr-scanner/app/_layout.tsx
+++ simple-qr-scanner/app/_layout.tsx
@@ -1,24 +1,38 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+// import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+  
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+          headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen 
+        name="index" 
+        options={{ 
+          title: 'QR Scanner',
+          headerShown: true 
+        }} 
+      />
+
+      <Stack.Screen 
+        name="modal" 
+        options={{ 
+          presentation: 'modal',
+          title: 'App Information' 
+        }} 
+      />
+    </Stack>
   );
 }
